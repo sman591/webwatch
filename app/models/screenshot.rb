46012f227ecc73a5dcd3d7e27cabf93c1b1b901a
Stuart Olivera <stuart@stuartolivera.com>
2016-05-30 20:19:39 -0700

    Removed thumbnail style from screenshots

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,7 +1,7 @@
 class Screenshot < ApplicationRecord
   belongs_to :website
 
-  has_attached_file :image, styles: { medium: "348x348>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "348x348>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
 end
