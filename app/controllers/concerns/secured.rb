4976a7424e4dc70215e461a524f3c2938f8489be
Stuart Olivera <stuart@stuartolivera.com>
2017-02-26 00:44:54 -0500

    Fix redirection on logout

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -8,11 +8,14 @@ module Secured
   def logged_in_using_omniauth?
     unless current_user
       flash[:notice] = 'Please login to access that page.'
-      redirect_to '/'
+      redirect_to root_path
+      return false
     end
 
     unless Rodash.get(current_user, "extra.raw_info.user_metadata.admin")
-      raise ActionController::RoutingError.new('Not Found')
+      flash[:alert] = 'Please contact an administrator to access that page.'
+      redirect_to root_path
+      return false
     end
   end
 end
