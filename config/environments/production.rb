6d52da17392716fae3d54155c9cc270146fa2f54
Stuart Olivera <stuart@stuartolivera.com>
2017-03-05 18:40:07 -0500

    Configure Action Cable for produciton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://watch.oweb.co/cable'
+  config.action_cable.allowed_request_origins = [ 'https://watch.oweb.co' ]
 
   # Don't mount Action Cable in the main server process.
   # config.action_cable.mount_path = nil
